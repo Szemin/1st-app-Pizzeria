d266cb7e2fd91051792b5ee48eab8cf61e90663d
Michał Kucharczyk <116577945+Szemin@users.noreply.github.com>
2022-11-28 20:57:20 +0100

    try catch first attempt

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("podaj numer");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("podaj numer2");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(num1 / num2);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+
+        
+    }
+}
