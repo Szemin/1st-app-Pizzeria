3d63a1458c9b9c248f5c760901eace6b43513f50
Michał Kucharczyk <116577945+Szemin@users.noreply.github.com>
2022-12-04 23:01:44 +0100

    void how to use

---
+++ mr white method void.cs
@@ -0,0 +1,15 @@
+﻿ string name;
+ Console.WriteLine("say my name");
+ name = Console.ReadLine();
+
+for (int i = 1; i <= 5; i++)
+{
+    SayHi(name);
+}
+ Console.WriteLine();
+
+static void SayHi(string name)
+{
+    Console.WriteLine("hello " + name);
+    Console.WriteLine("ye you god damn right");
+}
