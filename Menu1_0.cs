ef6a99d886cbf76fdce1588776c3c5b59f111533
Michał Kucharczyk <116577945+Szemin@users.noreply.github.com>
2022-12-05 12:36:37 +0100

    Pizzeria Menu 1.0

---
+++ Menu1_0.cs
@@ -0,0 +1,22 @@
+﻿class Menu
+{
+    
+    static void Main(string[] args)
+    {
+         Pizza();
+    }
+
+    static string[] Pizza()
+    {
+        string[] menu = { "1. margarita ", "2. hawajska ", "3. cappriciosa ", "4. dżalapino ", "5. ciasto z sosem " };
+        foreach (string i in menu)
+        {
+            Console.WriteLine(i);
+        }
+
+
+        return menu;
+    }
+
+
+}
