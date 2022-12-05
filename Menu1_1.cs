7256f7e0d185a4e0aba09e4e902eb8c13783e8ad
Michał Kucharczyk <116577945+Szemin@users.noreply.github.com>
2022-12-05 13:04:58 +0100

    Menu 1.1

---
+++ Menu1_1.cs
@@ -0,0 +1,39 @@
+﻿class Menu
+{  
+    static void Main(string[] args)
+    {
+         Pizza();
+        Console.WriteLine();
+            Pov();
+    }
+
+    static string[] Pizza()
+    {
+        Console.WriteLine(" Pizza: lista ");
+        string[] menu = { "1. margarita ", "2. hawajska ", "3. cappriciosa ", "4. dżalapino ", "5. ciasto z sosem " };
+        foreach (string i in menu)
+        {
+            Console.WriteLine(i);
+        }
+
+        return menu;
+    }
+
+    static void Pov()
+    {
+        Console.WriteLine("  ^    -----MENU-----    ^  ");
+        Console.WriteLine("----------------------------");
+        Console.WriteLine("         1. Info");
+        Console.WriteLine("         2. Info");
+        Console.WriteLine("         3. Info");
+        Console.WriteLine("         4. Info");
+        Console.WriteLine("         5. Info");
+        Console.WriteLine("----------------------------");
+
+    }
+
+    static void dodajPizze()
+    {
+      
+    }
+}
