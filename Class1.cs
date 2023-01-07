ccd747fb554a1763752efb897cecaeb16a8ba8be
Michał Kucharczyk <116577945+Szemin@users.noreply.github.com>
2023-01-07 15:22:25 +0100

    Cena

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    public class Cennik
+    {
+       public static void PrizeS()
+        {
+            Console.WriteLine("15zł");
+        }
+       public static void PrizeM()
+        {
+            Console.WriteLine("21,37zł");
+        }
+       public static void PrizeL()
+        {
+            Console.WriteLine("35zł");
+        }
+    }
+}
