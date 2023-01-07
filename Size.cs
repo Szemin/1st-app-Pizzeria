9c28de64933ded036fe5383f74a98e5c7ef7da26
Michał Kucharczyk <116577945+Szemin@users.noreply.github.com>
2023-01-07 16:36:35 +0100

    Class Size
    pizzas sizes

---
+++ Size.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    public class Size
+    {
+      
+       public static void info1()
+        {
+            Console.WriteLine(" \n");
+            Console.WriteLine(" Small pizza is 15x15 cm \n");
+        }
+      public static void info2()
+        {
+            Console.WriteLine(" \n");
+            Console.WriteLine(" Medium pizza is 21,37x21,37 cm \n");
+        }
+              
+      public static void info3()
+        {
+            Console.WriteLine(" \n");
+            Console.WriteLine("Large pizza is 33x33 cm \n");
+        }
+                 
+             
+    }
+}
