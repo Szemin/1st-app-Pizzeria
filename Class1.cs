70d75be5143c3ce211960a8e24f1565229aaf2b7
Michał Kucharczyk <116577945+Szemin@users.noreply.github.com>
2023-01-07 16:33:11 +0100

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -6,19 +6,34 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp6
 {
-    public class Cennik
-    {
-       public static void PrizeS()
-        {
-            Console.WriteLine("15zł");
-        }
-       public static void PrizeM()
-        {
-            Console.WriteLine("21,37zł");
-        }
-       public static void PrizeL()
-        {
-            Console.WriteLine("35zł");
+    public static void PrizeS()
+        { 
+            start:
+            Console.WriteLine("Small - 15zł" + "\n");
+        
+            Console.WriteLine("Medium - 21,37zł" + "\n");
+        
+            Console.WriteLine("Large - 35zł" + "\n" );
+
+            Console.WriteLine("choose your pizza size" + "\n");
+            string size = Console.ReadLine();
+            Console.WriteLine(" \n");
+            Console.WriteLine($"wybrałeś/aś opcję {size, 1} czy chcesz kontynuować? \n");
+            Console.WriteLine("Tak - przejdź do koszyka / Nie - wybierz ponownie");
+
+            var Uruchom = Console.ReadLine();
+            if (Uruchom == "Nie" || Uruchom =="nie")
+            {
+                goto start;
+            }
+            else if (Uruchom == "Tak" || Uruchom == "tak")
+            {
+
+
+
+            }
+
+
         }
     }
 }
